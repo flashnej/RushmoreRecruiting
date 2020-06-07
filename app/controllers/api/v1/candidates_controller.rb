6a810b5142016d2911dbd92825ec6987db0a8c68
flashnej <flashnej@gmail.com>
2020-06-07 19:05:11 -0400

    added some front end error handling, need to firgure out why second flash message doesnt show up

--- app/controllers/api/v1/candidates_controller.rb
+++ app/controllers/api/v1/candidates_controller.rb
@@ -8,8 +8,14 @@ class Api::V1::CandidatesController < ApplicationController
     resume = params["resume"]
     user_info = JSON.parse(params["info"])
     candidate = Candidate.new(first_name: user_info["firstName"], last_name: user_info["lastName"], email: user_info["email"], resume: resume)
+    if resume.content_type === "application/pdf"
+      status = {status: 'ok'}
+    else
+      status = {status: 'error'}
+    end
+
     if candidate.save
-      render json: {status: "ok"}
+      render json: status
     end
   end
 end
--- src/component/JobSeakers/JobSeakers.js
+++ src/component/JobSeakers/JobSeakers.js
@@ -12,47 +12,53 @@ const JobSeakers = () => {
 
   let formData = new FormData();
 
+    const handleSubmit = () => {
+      const droppackage = JSON.stringify({firstName: firstName, lastName: lastName, email: email});
+      formData.append('resume', resume)
+      formData.append('info', droppackage)
+      fetch(`http://localhost:5000/api/v1/candidates`, {
+        method: "POST",
+        body: formData,
+      })
 
-  const success = () => {
-    setFlash(
-    <FlashMassage duration={5000}>
-      <p className= "flash-notice">Resume Uploaded successfully</p>
-    </FlashMassage>)
-  }
 
-  const handleSubmit = () => {
-          const droppackage = JSON.stringify({firstName: firstName, lastName: lastName, email: email});
-          formData.append('resume', resume)
-          formData.append('info', droppackage)
-          fetch(`http://localhost:5000/api/v1/candidates`, {
-                            method: "POST",
-                            body: formData,
-          })
-          .then((response) => {
-                  if (response.ok) {
-                            success()
-                            return response;
+      .then((response) => {
+        if (response.ok) {
+          return response;
+        } else {
 
-                  }
-          })
-          .then((response) => {console.log("finished")})
+        }
+      })
+      .then((response) => response.json())
+      .then((body) => {
+        if (body.status ==="ok") {
+          setFlash(
+            <FlashMassage duration={5000}>
+              <p className= "happy-notice">Resume Uploaded successfully</p>
+            </FlashMassage>)
+        } else {
+          setFlash(
+            <FlashMassage duration={5000}>
+              <p className= "sad-notice">File must be a PDF</p>
+            </FlashMassage>)
+        }
+      })
+    }
 
-  }
-
-	return (
-		<div className='ContactUs'>
-			<h1 className='title'>Contact Us</h1>
-			<h2>Hello</h2>
+    return (
+      <div className='ContactUs'>
+      <h1 className='title'>Contact Us</h1>
+      <h2>Hello</h2>
       {flash}
-			<div className="inputs">
-				<Input onChange={e => setFirstName(e.target.value)} placeholder='First Name' />
-				<Input onChange={e => setLastName(e.target.value)} placeholder='Last Name' />
-				<Input onChange={e => setEmail(e.target.value)} placeholder='Email' />
-				<Input type="file" onChange={e => setResume(e.target.files[0])} />
-			</div>
-			<Button onClick={() => handleSubmit()}>Submit</Button>
-		</div>
-	);
-};
+      <div className="inputs">
+      <Input onChange={e => setFirstName(e.target.value)} placeholder='First Name' />
+      <Input onChange={e => setLastName(e.target.value)} placeholder='Last Name' />
+      <Input onChange={e => setEmail(e.target.value)} placeholder='Email' />
+      <Input type="file" onChange={e => setResume(e.target.files[0])} />
+      </div>
+      <Button onClick={() => handleSubmit()}>Submit</Button>
+      </div>
+    );
+  };
 
-export default JobSeakers;
+  export default JobSeakers;
